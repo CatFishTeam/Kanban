66fee76651c8af54faf5c44e4e0fd449127d1ba8
scabiati <scabiati@cisco.com>
2017-03-09 12:17:38 +0100

    architecture de base

---
+++ resources/views/kanban.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Kanbans
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="kanban">
+        <article class="col-md-3 col-sm-6 col-xs-12" style="background-color: #8c8c8c; color:white; text-align: center; height: 100vh;">
+            <h2>
+                To Do
+            </h2>
+            <hr>
+        </article>
+        <article class="col-md-3 col-sm-6 col-xs-12" style="background-color: #00b5e2; color:white; text-align: center; height: 100vh;">
+            <h2>
+                In Progress
+            </h2>
+            <hr>
+        </article>
+        <article class="col-md-3 col-sm-6 col-xs-12" style="background-color: #8c3310; color:white; text-align: center; height: 100vh;">
+            <h2>
+                To Review
+            </h2>
+            <hr>
+        </article>
+        <article class="col-md-3 col-sm-6 col-xs-12" style="background-color: #408140; color:white; text-align: center; height: 100vh;">
+            <h2>
+                Done
+            </h2>
+            <hr>
+        </article>
+    </section>
+@endsection
