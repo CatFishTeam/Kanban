9e93acf87132dea66b9fdc15363a2f7bc19cd39c
Mayon Maël <mael.mayon@free.fr>
2017-03-10 09:01:32 +0100

    Add Seed before fix Migration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,29 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use \App\Models\Task;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+class DatabaseSeeder extends Seeder {
+
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call('TaskTableSeeder');
+
+        $this->command->info('User task seeded!');
     }
+
+}
+
+class TaskTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('tasks')->delete();
+
+        Task::create(['id' => '1', 'title' => 'To Do']);
+        Task::create(['id' => '2', 'title' => 'In Progress']);
+        Task::create(['id' => '3', 'title' => 'To Review']);
+        Task::create(['id' => '4', 'title' => 'Done']);
+    }
+
 }
